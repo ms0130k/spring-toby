4e56a15f3ef5c318844065ea24d2df96d5a9415b
문정하 <youngmin.kim19@cj.net>
2022-11-30 18:43:19 +0900

    calc template 학습

--- src/springbook/learningtest/template/Calculator.java
+++ src/springbook/learningtest/template/Calculator.java
@@ -6,25 +6,49 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
-    public int calcSum(String filepath) {
-        BufferedReader br = null;
-        int sum = 0;
-        try {
-            br = new BufferedReader(new FileReader(filepath));
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                sum += Integer.parseInt(line);
+    public int calcSum(String filepath) throws IOException {
+        LineCallback<Integer> callback = new LineCallback<Integer>() {
+            @Override
+            public Integer doSomethingWithLine(String line, Integer result) {
+                return result + Integer.parseInt(line);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (br != null) br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sum;
+        };
+        return template(filepath, 0, callback);
     }
-    
+
+    private <T> T template(String filepath, T init, LineCallback<T> callback)
+            throws IOException, FileNotFoundException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            String line = null;
+            T result = init;
+            while ((line = br.readLine()) != null) {
+                result = callback.doSomethingWithLine(line, result);
+            }
+            return result;
+        }
+    }
+
+    public int calcMultiply(String filepath) throws IOException {
+        LineCallback<Integer> callback = new LineCallback<Integer>() {
+            @Override
+            public Integer doSomethingWithLine(String line, Integer result) {
+                return result * Integer.parseInt(line);
+            }
+        };
+        return template(filepath, 1, callback);
+    }
+
+    public String calcConcatenate(String filepath) throws IOException {
+        LineCallback<String> callback = new LineCallback<String>() {
+            @Override
+            public String doSomethingWithLine(String line, String result) {
+                return result + line;
+            }
+        };
+        return template(filepath, "", callback);
+    }
+
+    interface LineCallback<T> {
+        T doSomethingWithLine(String line, T result);
+    }
 }
